15b72b9b2ff6492a65b98ef1bc655b6fe9d75d3e
fox_itleksandr <fox_it@bk.ru>
2022-01-13 16:15:39 +0300

    Add project files.

---
+++ Lab14/Animals.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    abstract public class Animal
+    {
+        public abstract string Name { get; set; }
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.Write(Name + " говорит ");
+            Say();
+        }
+
+        protected Animal()
+        {          
+                Name = "Безымянное животное";           
+        }      
+
+    }
+
+    public class Cat:Animal
+    {
+        private string _name;
+
+        public override string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                if(value.Length>0)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    throw new Exception("Имя не может быть пустым");
+                }
+            }
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+
+    }
+
+    public class Dog : Animal
+    {
+        private string _name;
+
+        public override string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                if (value.Length > 0)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    throw new Exception("Имя не может быть пустым");
+                }
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+    }
+}
---
+++ Lab14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Animals;
+
+namespace Lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.Name = "Мурка";
+            cat.Say();
+            cat.ShowInfo();
+
+            Console.WriteLine();
+
+            Dog dog = new Dog();
+            dog.Name = "Бобик";
+            dog.Say();
+            dog.ShowInfo();
+
+            Console.WriteLine();
+
+            //проверим конструктор абстрактного класса
+            Dog unit = new Dog();
+            unit.Say();
+            unit.ShowInfo();
+
+            Console.ReadKey();
+
+        }
+    }
+}
